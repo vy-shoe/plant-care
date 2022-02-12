15df68706ca3abc60b47e176f2c587e27fb14011
vy-shoe <vadimulam6@gatech.edu>
2022-02-12 18:45:50 -0500

    main.js functionality with timer

--- index.js
+++ index.js
@@ -3,5 +3,13 @@ var characterChosen;
 function selectCharacter(value) {  
     characterChosen = value;
     alert("You selected " + characterChosen + " as your desk plant pet!"); 
-    window.open("main.html")
+    window.open("main/main.html")
+} 
+
+function getCharacter() { 
+    if (characterChosen == "") {
+        return "No character chosen"
+    } else {
+        return characterChosen;
+    }
 } 
