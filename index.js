459d13b2746c7f45226396ea67c27b518a4c8192
vy-shoe <vadimulam6@gatech.edu>
2022-02-12 17:39:05 -0500

    add new page for main functionality

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ var characterChosen;
 
 function selectCharacter(value) {  
     characterChosen = value;
-    alert(characterChosen);  
-}  
+    alert("You selected " + characterChosen + " as your desk plant pet!"); 
+    window.open("main.html")
+} 
