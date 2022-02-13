2eca3ad9646efab53462405eacacecb7af00ffb6
vy-shoe <vadimulam6@gatech.edu>
2022-02-12 20:03:55 -0500

    timer with graphics synced

--- main/main.js
+++ main/main.js
@@ -1,32 +1,38 @@
-import { characterChosen } from './index.js';
-const PLANT = characterChosen;
+// import getCharacter from './index.js';
+// var plant = getCharacter();
 var watered = false;
 
-function setCharacter() {
-
-}
-
 function waterPlant() {
-    console.log(PLANT);
+    // console.log(plant);
     if (watered == true) {
-        alert("I'm currently schlurping still! 🪴💦");
+        alert("I'm currently schlurping! 🪴💦");
     } else {
-        alert("Thanks for hydrating me! Yum... 🤤🌱!")
+        alert("Thanks for hydrating me! Yum... 🤤🌱!");
     }
+    document.getElementById("image").src = "/assets/plants/succulent1.png"
     watered = true;
     startTimer();
+    document.getElementById("text").innerHTML = "Your plant is doing a great job– just like you :)";
 }
+
 
 function startTimer() {
     setTimeout(midPlant, 1000 * 5); //5 seconds
     setTimeout(dryPlant, 1000 * 10); //10 seconds
+    setTimeout(needsWater, 1000 * 15);
 }
 
 function midPlant() {
-    
+    document.getElementById("image").src = "/assets/plants/succulent2.png"
+    document.getElementById("text").innerHTML = "Your plant is staying hydrated, you should take a break and drink water"
 }
 
 function dryPlant() {
-    alert("Help me, I'm thirsty! 🥵.🌿")
+    document.getElementById("image").src = "/assets/plants/succulent3.png"
+    document.getElementById("text").innerHTML = "Your plant needs some attention!"
     watered = false;
+}
+
+function needsWater() {
+    alert("Help me, I'm thirsty! 🥵.🌿");
 }
